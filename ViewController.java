2691bcf09cc3bbbc20a14cb36f43b34e5def8be5
yogesh <12kumaryogesh>
2019-09-17 15:33:59 +0530

    Two files are added for eroor 101

---
+++ ViewController.java
@@ -0,0 +1,28 @@
+
+package com.nt.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+/**
+ * @author yogesh
+ *
+ */
+public class ViewController implements Controller {
+
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Date d=null;
+		d=new Date();
+		return new ModelAndView("home", "richa",d);
+		
+	}
+
+}
